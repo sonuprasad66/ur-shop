32ab94107dac171c49339ae6efa53d06b97ddbc4
agodse21 <agodse9@gmail.com>
2023-01-12 23:41:05 +0530

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import { Navbar } from "./Components/Navbar";
 import { AllRoutes } from "./Routes/AllRoutes";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <AllRoutes />
-    </div>
+    <>
+      <Box className="font-face-pt">
+        <Navbar />
+        <AllRoutes />
+      </Box>
+    </>
   );
 }
 
