da2f117eb11635d8f541ab524ba48b131a8f3fe2
sonuprasad66 <sp.srp1999@gmail.com>
2023-02-10 10:40:25 +0530

    sonu-day-2

--- backend/Routes/Products.route.js
+++ backend/Routes/Products.route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const productsRouter = express.Router();
-const { getAllProducts } = require("../Controllers/Products.controller");
+const {
+  getAllProducts,
+  getAllFilterProducts,
+} = require("../Controllers/Products.controller");
 
 productsRouter.get("/getAllProducts", getAllProducts);
+productsRouter.get("/getAllFilterProducts", getAllFilterProducts);
 
 module.exports = {
   productsRouter,
