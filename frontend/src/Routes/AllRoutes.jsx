a58d62acf109c66e8c019ca3642a436f1f7412cf
sonuprasad66 <sp.srp1999@gmail.com>
2023-02-11 13:38:48 +0530

    cart added

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Cart from "../cart/Cart";
 import { Route, Routes } from "react-router";
+import Home from "../Pages/Home";
 import { Login } from "../Components/Register/Login";
 import { Signup } from "../Components/Register/Signup";
-import Home from "../Pages/Home";
-import { ProductsDetails } from "../Pages/ProductsDetails/ProductsDetails";
 import { Products } from "../Pages/ProductsPage/Products";
+import { ProductsDetails } from "../Pages/ProductsDetails/ProductsDetails";
 
 export const AllRoutes = () => {
   return (
@@ -15,6 +16,7 @@ export const AllRoutes = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductsDetails />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
