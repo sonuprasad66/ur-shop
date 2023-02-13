419cc9a2c0794df2e4abaebdba9df361f2d59f27
sonuprasad66 <sp.srp1999@gmail.com>
2023-02-13 23:06:23 +0530

    all-category-done

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -10,12 +10,20 @@ import { CheckoutPage } from "../Pages/CheckoutPage";
 import { Wishlist } from "../Components/Wishlist/Wishlist";
 import { MyAccount } from "../Components/MyAccount/MyAccount";
 import { Team } from "../Components/ContactUs/Team";
+import { Mens } from "../Components/ProductCategory/Mens";
+import { Womens } from "../Components/ProductCategory/Womens";
+import { Kids } from "../Components/ProductCategory/Kids";
+import { Electronics } from "../Components/ProductCategory/Electronics";
 
 export const AllRoutes = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/mens" element={<Mens />} />
+        <Route path="/womens" element={<Womens />} />
+        <Route path="/kids" element={<Kids />} />
+        <Route path="/electronics" element={<Electronics />} />
         <Route path="/team" element={<Team />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
